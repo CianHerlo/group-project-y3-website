9948e57c5cd9510c3e2d830c095c7f2f196ac5bb
AaronCanty <aaron.canty@gmail.com>
2022-12-02 11:26:17 +0000

    payment integrated

---
+++ functions/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "google",
+  ],
+  rules: {
+    "quotes": ["error", "double"],
+    "indent": "off",
+  },
+  parserOptions: {
+    "ecmaVersion": 2017,
+  },
+};
