a48c590bb7afbd48e5ba722bf2f8c5ef64ad41e3
Alannah <a.cullinane-cooney@mycit.ie>
2022-12-11 04:26:44 +0000

    Created cart.js

--- public/cart.js
+++ public/cart.js
@@ -1,2 +1,8 @@
 //OPEN & CLOSE CART
-const cartIcon = document.querySelector("")
+const cartIcon = document.querySelector("#cart-icon");
+const cart = document.querySelector(".cart");
+const closeCart = document.querySelector("#cart-close");
+
+cartIcon.addEventListener('click', ()=>{
+    cart.classList.add('active');
+});
