62f52b19af6d8d68913f6a94fa782d47be56ae05
Alannah <a.cullinane-cooney@mycit.ie>
2022-12-11 05:13:26 +0000

    Added basic functions to cart.js

--- public/cart.js
+++ public/cart.js
@@ -6,3 +6,46 @@ const closeCart = document.querySelector("#cart-close");
 cartIcon.addEventListener('click', ()=>{
     cart.classList.add('active');
 });
+
+closeCart.addEventListener('click', ()=>{
+    cart.classList.remove('active');
+});
+
+//Start when the document is ready 
+if(document.readyState == "loading"){
+    document.addEventListener('DOMContentLoaded', start)
+}else{
+    start();
+}
+
+
+//=================== START ================================
+function start(){
+    addEvents();
+    updateTotal();
+
+}
+//=================== UPDATE & RERENDER======================
+function update(){
+    addEvents();
+
+}
+//=================== ADD EVENTS ======================
+function addEvents(){
+    // Remove items from cart
+    let cartRemove_btns = document.querySelectorAll('#cart-remove');
+    cartRemove_btns.forEach((btn) => {
+        btn.addEventListener("click", handle_removeCartItem);
+    });
+}
+//=================== HANDLE EVENTS FUNCTIONS ======================
+function handle_removeCartItem(){
+    this.parentElement.remove();
+
+    update();
+}
+//=================== UPDATE & RERENDER FUCNTIONS ======================
+function update(){
+    addEvents();
+
+}
